4fdaf17cca88eb8863aa7a46db1a57b34938b961
Christophe De Wagter <dewagter@gmail.com>
2016-12-15 21:10:10 +0100

    [validate] ISSN

--- validate.py
+++ validate.py
@@ -42,14 +42,18 @@ def find_url( line ):
         resp_code = checkUrl ( url )
         print( resp_code, "FILE", url )
     if "doi = " in line:
-        doi = bib_tag_content(line, "doi") 
+        doi = bib_tag_content(line, "doi")
         resp_code = checkUrl ( "https://doi.org/" + doi )
         print( resp_code, "DOI", doi )
     if "isbn = " in line:
-        isbn = bib_tag_content(line, "doi") 
+        isbn = bib_tag_content(line, "isbn")
         resp_code = checkUrl ( "http://isbndb.com/search/all?query=" + isbn )
         print( resp_code, "ISBN", isbn )
-
+    if "issn = " in line:
+        issn = bib_tag_content(line, "issn")
+        #resp_code = checkUrl ( "https://journals4free.com/?q=" + issn )
+        resp_code = checkUrl ( "https://journals4free.com/link.jsp?l=" + issn )
+        print( resp_code, "ISSN", issn )
 
 if __name__ == '__main__':
     for line in bibfile:
