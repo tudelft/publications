35602e85e4c8532659c71f4c73cee70d869b10a7
Christophe De Wagter <dewagter@gmail.com>
2017-11-01 13:23:00 +0100

    Validate also when spacing is not correct

--- validate.py
+++ validate.py
@@ -4,7 +4,12 @@
 import httplib
 from urlparse import urlparse
 
+count_url = 0
+count_good = 0
+
 def checkUrl(url):
+    global count_url, count_good
+    count_url += 1
     try:
         p = urlparse(url)
         conn = httplib.HTTPConnection(p.netloc)
@@ -12,6 +17,8 @@ def checkUrl(url):
         resp = conn.getresponse()
         if resp.status >= 400:
             print ("ERROR: link to " + url + " is failing.")
+        else:
+            count_good += 1
         if (resp.status >= 301) & (resp.status <= 303):
             # MOVED:
             new_url = resp.getheader('location')
@@ -33,8 +40,11 @@ def noaccent( txt ):
     return ''.join(x for x in unicodedata.normalize('NFKD', txt) if x in string.ascii_letters).lower()
 
 
+def bib_tag( line):
+    return line.split("=", 1)[0].strip();
+
 def bib_tag_content( line):
-    return unlatex(line.split("=", 1)[-1].strip().lstrip("=").rstrip(",").strip().lstrip("{").rstrip("}").strip());
+    return unlatex(line.split("=", 1)[-1].strip().lstrip("=").rstrip(",").strip().lstrip("\"").rstrip("\"").lstrip("{").rstrip("}").strip());
 
 
 with open("mavlab.bib") as ft:
@@ -42,34 +52,38 @@ with open("mavlab.bib") as ft:
 
 
 def find_url( line ):
-    if "url = " in line:
-        url = bib_tag_content(line) 
-        resp_code = checkUrl ( url )
-        print( resp_code, "URL", url )
-    if "pdf = " in line:
-        url = bib_tag_content(line) 
-        resp_code = checkUrl ( url )
-        print( resp_code, "PDF", url )
-    if "file = " in line:
-        url = bib_tag_content(line) 
-        resp_code = checkUrl ( url )
-        print( resp_code, "FILE", url )
-    if "doi = " in line:
-        doi = bib_tag_content(line)
-        resp_code = checkUrl ( "https://doi.org/" + doi )
-        print( resp_code, "DOI", doi )
-    if "isbn = " in line:
-        isbn = bib_tag_content(line)
-        resp_code = checkUrl ( "http://isbndb.com/search/all?query=" + isbn )
-        print( resp_code, "ISBN", isbn )
-    if "issn = " in line:
-        issn = bib_tag_content(line)
-        #resp_code = checkUrl ( "https://journals4free.com/?q=" + issn )
-        resp_code = checkUrl ( "https://journals4free.com/link.jsp?l=" + issn )
-        print( resp_code, "ISSN", issn )
+    if "=" in line:
+        tag = bib_tag(line).lower()
+        #print("TAG:", tag)
+        if tag == "url":
+            url = bib_tag_content(line) 
+            resp_code = checkUrl ( url )
+            print( resp_code, "URL", url )
+        if tag == "pdf":
+            url = bib_tag_content(line) 
+            resp_code = checkUrl ( url )
+            print( resp_code, "PDF", url )
+        if tag == "file":
+            url = bib_tag_content(line) 
+            resp_code = checkUrl ( url )
+            print( resp_code, "FILE", url )
+        if tag == "doi":
+            doi = bib_tag_content(line)
+            resp_code = checkUrl ( "https://doi.org/" + doi )
+            print( resp_code, "DOI", doi )
+        if tag == "isbn":
+            isbn = bib_tag_content(line)
+            resp_code = checkUrl ( "http://isbndb.com/search/all?query=" + isbn )
+            print( resp_code, "ISBN", isbn )
+        if tag == "issn":
+            issn = bib_tag_content(line)
+            #resp_code = checkUrl ( "https://journals4free.com/?q=" + issn )
+            resp_code = checkUrl ( "https://journals4free.com/link.jsp?l=" + issn )
+            print( resp_code, "ISSN", issn )
 
 if __name__ == '__main__':
     for line in bibfile:
         find_url( line )
+    print("Report: ", count_good, " good out of ", count_url)
     #print checkUrl('http://www.stackoverflow.com') # True
     #print checkUrl('http://stackoverflow.com/notarealpage.html') # False
