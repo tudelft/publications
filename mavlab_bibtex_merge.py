d629368ad0efed4fbd913f7015e9e61c851f5ed5
Christophe De Wagter <c.dewagter@tudelft.nl>
2021-02-16 17:24:41 +0100

    extra media coverage

--- mavlab_bibtex_merge.py
+++ mavlab_bibtex_merge.py
@@ -7,26 +7,28 @@ with open('cs_mav.bib', encoding="utf8") as bibtex_file:
     bibtex_str = bibtex_file.read()
 
 bib_pure = bibtexparser.loads(bibtex_str, parser=parser)
-print(len(bib_pure.entries))
+print('cs_mav.bib contains ',len(bib_pure.entries), ' entries')
 
 # Import WEBSITE
 parser2 = bibtexparser.bparser.BibTexParser(common_strings=True)
-with open('website.bib', encoding="utf8") as bibtex_file2:
+with open('website_export.bib', encoding="utf8") as bibtex_file2:
     bibtex_str2 = bibtex_file2.read()
 
 bib_website = bibtexparser.loads(bibtex_str2, parser=parser2)
+print('website_export.bib contains ',len(bib_website.entries), ' entries')
 
 # Results
 mavlab_missing = bibtexparser.bibdatabase.BibDatabase()
 mavlab_merged  = bibtexparser.bibdatabase.BibDatabase()
 
-print(len(bib_pure.entries),len(bib_website.entries))
+verbose = False
 
 def cleanup_title(txt):
     txt = txt.replace('{', '').replace('}', '').strip().lower()
     txt = txt.replace(',', ' ').replace('.', ' ').replace('`', ' ').replace('?', ' ')
-    txt = txt.replace('\\textquoteright','').replace('\\textquoteleft','')
-    txt = txt.replace('/', ' ').replace('-', ' ').replace('\'', ' ').replace('"', ' ').replace(':', ' ')
+    txt = txt.replace('\\textquoteright','').replace('\\textquoteleft','').replace('{}','')
+    txt = txt.replace('textquoteright',' ').replace('textquoteleft',' ')
+    txt = txt.replace('/', ' ').replace('-', '').replace('\'', ' ').replace('"', ' ').replace(':', ' ')
     txt = txt.replace('  ', ' ').replace('  ', ' ').replace('  ', ' ').replace('  ', ' ').strip()
     return txt
 
@@ -46,20 +48,24 @@ for b in bib_pure.entries:
 
     merged = 0
     for b2 in bib_website.entries:
-            
+        
+        # Prefer matching DOI
         if 'doi' in b2 and 'doi' in b:
             if b['doi'] == b2['doi']:
-                print('-merged',b['ID'],b2['ID'])
+                if verbose:
+                    print('-DOI merged',b['ID'],b2['ID'])
                 mavlab_merged.entries.append(b)
                 bib_website.entries.remove(b2)
                 merged = 1
                 break
 
-        if 'title' in b2 and 'ENTRYTYPE' in b2:
+        # Or match title and entrytype and year
+        if 'title' in b2 and 'ENTRYTYPE' in b2 and 'year' in b2:
             t1 = cleanup_title(b['title'])
             t2 = cleanup_title(b2['title'])
-            if t1 == t2 and b['ENTRYTYPE'] == b2['ENTRYTYPE']:
-                print('-merged',b['ID'],b2['ID'])
+            if t1 == t2 and b['ENTRYTYPE'] == b2['ENTRYTYPE'] and b['year'] == b2['year']:
+                if verbose:
+                    print('-merged',b['ID'],b2['ID'])
                 mavlab_merged.entries.append(b)
                 bib_website.entries.remove(b2)
                 merged = 1
@@ -69,7 +75,8 @@ for b in bib_pure.entries:
         mavlab_missing.entries.append(b)
 
 
-print(len(mavlab_missing.entries),len(mavlab_merged.entries))
+print('pure',len(bib_pure.entries), ' split in merged ', len(mavlab_merged.entries), ' missing ',len(mavlab_missing.entries))
+print('website entries remaining', len(bib_website.entries))
 
 # dump back
 writer = bibtexparser.bwriter.BibTexWriter()
