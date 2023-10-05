b630de9fffffddf91cb2c6c521f60ba77dea0fa9
Christophe De Wagter <dewagter@gmail.com>
2023-10-05 10:50:32 +0200

    mavlab summary

--- filter_pure_mavlab.py
+++ filter_pure_mavlab.py
@@ -134,7 +134,7 @@ def print_summary():
             fout.write(';' + t)
         fout.write('\n')
 
-        for y in range(2003,2023):
+        for y in range(2003,2024):
             fout.write(str(y) + ';')
             for t in paper_types:
                 key = (str(y),t)
